f911dbe98bd556ca0985bc56242cd6eb4dc226b7
I'm Bharadwaj <bravebharadwaj31@gmail.com>
2024-01-01 22:01:55 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,155 @@
+export const Bio = {
+  name: "Bharadwaj",
+  roles: [
+    "Front End Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Bharadwaj310701",
+  resume:
+    "https://drive.google.com/file/d/13mOG0jWlVber09VSa78-sXk_fMP7A1lv/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/bharadwaj07",
+  twitter: "https://twitter.com/Retchi31",
+  insta: "https://www.instagram.com/_007_bharadwaj/",
+  facebook: "https://www.facebook.com/brave.bharad",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+    ],
+  },
+ 
+  {
+    title: "Others",
+    skills: [
+      
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+     
+     
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+    role: "Frontend Engineering Intern",
+    company: "Zaapee Pvt. Ltd.",
+    date: "Oct 2023 - Nov 2023",
+    desc: " The Project  is about personal protfolio site using Javascript, Tailwindcss.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Tailwindcss",
+      "JavaScripte",
+      "Firebase"
+      ],
+      github: "https://github.com/rishavchanda/Trackify",
+      webapp: "https://trackify.duckdns.org",
+  },
+  {
+    id: 0,
+    img: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+    role: "Frontend Engineer Intern",
+    company: "Zaapee Pvt. Ltd.",
+    date: "Nov 2023 - Dec 2023",
+    desc: "Working on the frontend of the web application using ReactJS,TypeScripte and JavaScripte.",
+    skills: [
+      "ReactJS",
+      "TypeScripte",
+      "JavaScripte",
+      "HTML",
+      "CSS",
+      "Firebase"
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    
+    img: "https://drive.google.com/file/d/1Ke2w9KMLvS35JtEj_CLJvZDZKKEbM7Ia/view?usp=drive_link",
+    school: "Swami Vivekanandha Matriculation Higher Secondary School",
+    date: "Jun 2018 - Apr 2018",
+    grade: "70.3%",
+    desc: "I completed my class 12 high school education at SVMS Hrs School, Kanchipuram, where I studied Computer Science.",
+    degree: "HSLC(XII),ComputerScience ",
+  },
+  
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Swami Vivekanandha Matriculation Higher Secondary School",
+    date: "Jun 2016 - Apr 2016",
+    grade: "88.2%",
+    desc: "I completed my class 10 education at SVMS Hrs School, Kanchipuram, where I studied Computer Application.",
+    degree: "SSLC(X)",
+  },
+  {
+    id: 2,
+    img: "",
+    school: " Sri Chandrasekharendra Saraswathi Viswa Mahavidyalaya (SCSVMV)",
+    date: "Apr 2017 - Apr 2019",
+    grade: "82.3%",
+    desc: "I completed a Bachelor's degree in Civil & Structural Engineering at Sri Chandrasekharendra Saraswathi Viswa Mahavidyalaya (SCSVMV), Kanchipuram. I've a CGPA of 8.71. I have taken courses in Estimation Of Building , Environmental Engg,Base Structures , program For Problem Solving and Strength Of Materials.",
+    degree: "BE.Civil & Structural Engineering",
+  },
+];
+
